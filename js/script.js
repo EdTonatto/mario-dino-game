a43028e489887040111242e34a15c42d115a002e
Eduardo Tonatto <edtonatto@gmail.com>
2023-04-11 15:01:44 -0300

    :construction: Added gameover condition

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -6,6 +7,16 @@ const jump = () => {
         mario.classList.remove('jump');
     }, 500);
 };
+
+const loop = setInterval(() => {
+    const pipeLeftPosition = pipe.offsetLeft;
+    const marioBottomPosition = Number(window.getComputedStyle(mario).bottom.replace('px', ''));
+
+    if(pipeLeftPosition > 0 && pipeLeftPosition <= 70 && marioBottomPosition <= 60) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipeLeftPosition}px`;
+    }
+}, 10)
 
 document.addEventListener('keydown', jump);
 document.addEventListener('touchstart', jump);
