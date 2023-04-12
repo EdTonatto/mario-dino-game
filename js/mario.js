13e8279c5890a3db804cdd1f4f2350f86a3674f9
Eduardo Tonatto <edtonatto@gmail.com>
2023-04-12 13:24:21 -0300

    :construction: Added jump events and control

--- js/mario.js
+++ js/mario.js
@@ -3,6 +3,12 @@ class Mario {
     walkAnimationTimer = this.WALK_ANIMATION_TIMER;
     dinoRunImages = [];
 
+    jumpPressed = false;
+    jumpInProgress = false;
+    falling = false;
+    JUMP_SPEED = 0.6;
+    GRAVITY = 0.4;
+
     constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio) {
         this.ctx = ctx;
         this.canvas = ctx.canvas;
@@ -14,6 +20,7 @@ class Mario {
 
         this.x = 15 * scaleRatio;
         this.y = this.canvas.height - this.height - 1.5 * scaleRatio;
+        this.yStandingPosition = this.y;
 
         this.aliveMarioImage = new Image();
         this.aliveMarioImage.src = '../images/mario-stand-still.png';
@@ -27,6 +34,37 @@ class Mario {
 
         this.dinoRunImages.push(marioRunImage1);
         this.dinoRunImages.push(marioRunImage2);
+
+        window.removeEventListener("keydown", this.keydown);
+        window.removeEventListener("keyup", this.keyup);        
+        window.addEventListener("keydown", this.keydown);
+        window.addEventListener("keyup", this.keyup);
+
+        window.removeEventListener('touchstart', this.touchstart);
+        window.removeEventListener('touchend', this.touchend);
+        window.addEventListener('touchstart', this.touchstart);
+        window.addEventListener('touchend', this.touchend);
+
+    }
+
+    keydown = (event) => {
+        if(event.code === 'Space') {
+            this.jumpPressed = true;
+        }
+    }
+
+    keyup = (event) => {
+        if(event.code === 'Space') {
+            this.jumpPressed = false;
+        }
+    }
+
+    touchstart = () => {
+        this.jumpPressed = true;
+    }
+
+    touchend = () => {
+        this.jumpPressed = false;
     }
 
     run(gameSpeed, frameTimeDelta) {
@@ -41,8 +79,33 @@ class Mario {
         this.walkAnimationTimer -= frameTimeDelta * gameSpeed;
     }
 
+    jump(frameTimeDelta) {
+        if(this.jumpPressed){
+            this.jumpInProgress = true;
+        }
+
+        if(this.jumpInProgress && !this.falling) {
+            if(this.y > this.canvas.height - this.minJumpHeight || this.y > this.canvas.height - this.maxJumpHeight && this.jumpPressed) {
+                this.y -= this.JUMP_SPEED * frameTimeDelta * this.scaleRatio;
+            } else {
+                this.falling = true;
+            }
+        } else {
+            if(this.y < this.yStandingPosition) {
+                this.y += this.GRAVITY * frameTimeDelta * this.scaleRatio;
+                if(this.y + this.height > this.canvas.height) {
+                    this.y = this.yStandingPosition
+                }
+            } else {
+                this.falling = false;
+                this.jumpInProgress = false;
+            }
+        }
+    }
+
     update(gameSpeed, frameTimeDelta) {
-        this.run(gameSpeed, frameTimeDelta)
+        this.run(gameSpeed, frameTimeDelta);
+        this.jump(frameTimeDelta);
     }
 
     draw() {
